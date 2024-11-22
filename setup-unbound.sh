a1a1323c9f17da3912d862f8a6f9dad634aada3f
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2024-11-22 15:36:56 -0700

    Update setup-unbound.sh

--- setup-unbound.sh
+++ setup-unbound.sh
@@ -1,5 +1,5 @@
 # curl -s https://raw.githubusercontent.com/Null-Foundation/scripts/refs/heads/main/setup-unbound.sh | sh
-apt-get install unbound -y;
+apt-get install unbound dnsutils -y;
 cp /etc/resolv.conf /etc/resolv.conf.backup;
 echo nameserver 127.0.0.1 > /etc/resolv.conf;
 chattr +i /etc/resolv.conf;
