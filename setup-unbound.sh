5d7613445aa3833f4d36be542225e79308b26cdf
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2024-11-22 15:36:14 -0700

    Update setup-unbound.sh

--- setup-unbound.sh
+++ setup-unbound.sh
@@ -1,3 +1,4 @@
+# curl -s https://raw.githubusercontent.com/Null-Foundation/scripts/refs/heads/main/setup-unbound.sh | sh
 apt-get install unbound -y;
 cp /etc/resolv.conf /etc/resolv.conf.backup;
 echo nameserver 127.0.0.1 > /etc/resolv.conf;
